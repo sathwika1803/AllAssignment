ec5ebc6a849d348570d462190a0955427d489237
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-04-02 20:48:26 +0530

    operatorstask

---
+++ operators.java
@@ -0,0 +1,21 @@
+public class operators{
+   
+    
+
+
+public static void main(String[]args){
+   int a=10;
+   int b=5;
+   int sum=a+b;
+   int sub=a-b;
+   int mul=a*b;
+   int div=a/b;
+   int module=a%b;
+   System.out.println("sum:"+sum);
+   System.out.println("sub:"+sub);
+   System.out.println("mul:"+mul);
+   System.out.println("div:"+div);
+   System.out.println("module:"+module);
+
+}
+}
