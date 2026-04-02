cc04bf392923569a9d3c1f83c06df44aa76ce8bd
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-04-02 21:49:26 +0530

    operators

---
+++ arithmetic.java
@@ -0,0 +1,29 @@
+public class arithmetic {
+    public static void main(String[] args) {
+        double a=10;
+        double b=20;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a<b);
+        System.out.println(a>b);
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+        System.out.println((a==b)&&(a<b));
+        System.out.println((a!=b)&&(a>b));
+        System.out.println((a==b)||(a<b));
+        System.out.println((a!=b)||(a>b));
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(++a);
+        System.out.println(a--);
+        System.out.println(a);
+        System.out.println(--a);
+
+        
+    }
+}
