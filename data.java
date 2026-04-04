1e10a75024d1bd66c58be6b492bbea49e551d520
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-04-04 14:04:35 +0530

    datatype

---
+++ data.java
@@ -0,0 +1,30 @@
+public class data{
+    public static void main(String[] args){
+        String name="sathwika";
+        int age=21;
+        double height=4.7;
+        boolean isname=true;
+        char section='B';
+        char grade='A';
+        String hallticketno="236B1A0596";
+        double percentage=9.8;
+        boolean iseligibleforvote=true;
+        double weight= 48.5;
+        long phnnu=6281310200L;
+         System.out.println("Name  :"+name);
+         System.out.println("age  :"+age);
+         System.out.println("height  :"+height);
+         System.out.println("Name  :"+isname);
+         System.out.println("Section  :"+section);
+         System.out.println("Grade  :"+grade);
+         System.out.println("Hallticket  :"+hallticketno);
+         System.out.println("Percentage  :"+percentage);
+         System.out.println("VoteEligible :"+iseligibleforvote);
+         System.out.println("Weight  :"+weight);
+          System.out.println("Phno  :"+phnnu);
+         
+
+
+
+    }
+}
