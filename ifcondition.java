d60251afe58110d2a08065c13ba229121194ca85
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-04-08 22:33:02 +0530

    ifff

--- ifcondition.java
+++ ifcondition.java
@@ -1,11 +1,23 @@
-public class ifcondition{
-    static boolean pass= true;
-
-    public static void main(String[] args){
-        if (pass){
-            System.out.println("Congratulations your pass");
-            
-
+public class ifcondition {
+    static int number=200;
+    public static void main(String[] args) {
+        if (number>0){
+            System.out.println("Positive number");
+        }
+        if (number<0){
+            System.out.println("negative number");
+        }
+        if (number==0){
+            System.out.println("Zero");
+        }
+        if (number%2==0){
+            System.out.println("even Number");
+        }
+        if (number%2==1){
+            System.out.println("odd number");
+        }
+        if (number>100){
+            System.out.println("large number");
         }
     }
 }
