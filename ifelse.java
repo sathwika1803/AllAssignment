c2aeabb794a0050a344365ba87c1cb2212cf6e90
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-04-08 22:39:27 +0530

    ifelse

---
+++ ifelse.java
@@ -0,0 +1,11 @@
+public class ifelse {
+    static int age=20;
+    public static void main(String[] args) {
+        if (age>=18){
+            System.out.println("eligible for vote");
+        }
+        else{
+            System.out.println("not eligible for vote");
+        }
+    }
+}
