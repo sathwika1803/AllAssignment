e5add96db8854d145d00240d974f8f68e5367af6
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-04-08 22:36:47 +0530

    iffff

---
+++ ifff.java
@@ -0,0 +1,14 @@
+public class ifff {
+    static int number=35;
+    public static void main(String[] args) {
+        if (number>=35){
+            System.out.println("your pass");
+            System.err.println("your can enjoy");
+        }
+        if (number>=35)
+            System.out.println("your pass");
+            System.err.println("your can enjoy");
+            
+        
+    }
+}
