22aac1b7ad213dd41e856fb07470042f963b25b9
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-04-08 22:04:27 +0530

    else

---
+++ elsecondition.java
@@ -0,0 +1,12 @@
+public class elsecondition {
+    static int number=20;
+    public static void main(String[] args) {
+        if (number>=18){
+            System.out.println("eligible for voting");
+        }
+        else{
+            System.out.println("not eligible for vote");
+        }
+    }
+    
+}
