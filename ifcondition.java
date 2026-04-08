aba71a83459c3cf87999df96a127ec0ed6449667
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-04-08 21:54:37 +0530

    ifcond

---
+++ ifcondition.java
@@ -0,0 +1,11 @@
+public class ifcondition{
+    static boolean pass= true;
+
+    public static void main(String[] args){
+        if (pass){
+            System.out.println("Congratulations your pass");
+            
+
+        }
+    }
+}
