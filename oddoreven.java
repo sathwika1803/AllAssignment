bf43fb35c1de5d9e068bece33247512de58897ba
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-04-08 22:41:50 +0530

    evenoddnumbers

---
+++ oddoreven.java
@@ -0,0 +1,9 @@
+public class oddoreven {
+    static int digit=18;
+    public static void main(String[] args) {
+        if (digit%2==0)
+            System.out.println("even number");
+        else
+            System.out.println("odd number");
+    }
+}
