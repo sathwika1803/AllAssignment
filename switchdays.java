2dc1c2c6ef340106d35c677ec9f206bc6756c05f
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-04-12 19:38:14 +0530

    switchdays

---
+++ switchdays.java
@@ -0,0 +1,22 @@
+public class switchdays {
+    static char grade =2;
+    public static void main(String[] args) {
+        switch(grade){
+            case 1:
+                System.out.println("Excellent");
+                break;
+            case 2:
+                System.out.println("Good");
+                break;
+            case 3:
+                System.out.println("Average");
+                break;
+            case 4:
+                System.out.println("Below Average");
+                break;
+            case 5:
+                System.out.println("Fail");
+
+        }
+    }
+}
