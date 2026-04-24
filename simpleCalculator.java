c0bc2651796a476f49c0ee037d0e96a327fa6c60
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-04-24 07:43:53 +0530

    simpleCalculator

---
+++ simpleCalculator.java
@@ -0,0 +1,24 @@
+public class simpleCalculator {
+    static int a=10;
+    static int b=20;
+    static String operator="+";
+    public static void main(String[] args) {
+        switch(operator){
+            case "+":
+                System.out.println(a+b);
+                break;
+            case "-":
+                System.out.println(a-b);
+                break;
+            case "*":
+                System.out.println(a*b);
+                break;
+            case "/":
+                System.out.println(a/b);
+                
+        }
+
+
+
+    }
+}
