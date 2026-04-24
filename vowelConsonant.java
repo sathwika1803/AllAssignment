e960327551468430a53f69380c54b32ced4f984b
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-04-24 07:51:01 +0530

    vowelConsonant

---
+++ vowelConsonant.java
@@ -0,0 +1,16 @@
+public class vowelConsonant {
+    static String alpha="z";
+    public static void main(String[] args) {
+        switch(alpha){
+            case "a":
+            case "e":
+            case "i":
+            case "o":
+            case "u":
+           System.out.println("Vowels");
+           break;
+           default:
+            System.out.println("Consonant");
+        }
+    }
+}
