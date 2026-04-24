f3e33d4883d814c8d085d553b3f7e16a2dbdbd5c
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-04-24 08:10:17 +0530

    forLoop

---
+++ forLoop.java
@@ -0,0 +1,17 @@
+public class forLoop {
+    
+   
+    public static void main(String[] args) {
+        System.out.println("Numbers From 1 to 10");
+        for (int i=1;i<=10;i++){
+            System.out.println(i);
+        }
+        System.out.println("Numbers From 10 to 1");
+        for (int i=10;i>=1;i--){
+            System.out.println(i);
+        }
+
+        
+        }
+}
+
