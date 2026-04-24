f2d66f5f8e492cdd52819aaba94b765b60e7837c
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-04-24 08:01:35 +0530

    logincheck

---
+++ loginCheck.java
@@ -0,0 +1,14 @@
+public class loginCheck {
+    static String username = "Sathwika";
+    static String password= "SathwikaGupta";
+    public static void main(String[] args) {
+        if(username.equals("sathwika") && (password.equals("SathwikaGupta"))){
+        System.out.println("Login Successful");
+        }
+        else{
+System.out.println("Invalid username and password");
+        }
+        
+    }
+
+}
