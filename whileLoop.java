b4d3edd2451da6e333e1e8386bba5101d417fa1c
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-04-24 08:23:52 +0530

    whileLoop

---
+++ whileLoop.java
@@ -0,0 +1,17 @@
+public class whileLoop {
+    public static void main(String[] args) {
+        System.out.println("Numbers from 0-10");
+       int i = 0;
+       while (i<=10){
+        System.out.println(i);
+        i++;
+       }
+       System.out.println("Numbers from 10-1");
+       int a = 10;
+         while (a >= 1){
+        System.out.println(a);
+        a--;
+       }
+
+    }
+}
