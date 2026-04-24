2c7d4dc7ea364968876c5ab57e8f8dbbbfe891f8
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-04-24 11:41:46 +0530

    forWhileDo

---
+++ forWhileDo.java
@@ -0,0 +1,17 @@
+public class forWhileDo {
+    public static void main(String[] args) {
+        for (int i=0;i<0;i++){
+            System.out.println(i);
+        }
+        int i=0;
+        while (i<0){
+            System.out.println(i);
+            i++;
+        }
+        int a=1;
+        do{
+            System.out.println(a);
+            a++;
+        }while(a<0);
+    }
+}
