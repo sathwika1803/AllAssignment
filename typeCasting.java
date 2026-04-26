9c58f1b66702fbf97d0364a60b3cecc723f5f48a
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-04-26 22:02:12 +0530

    typecasting

---
+++ typeCasting.java
@@ -0,0 +1,15 @@
+public class typeCasting {
+    public static void main(String[] args) {
+        int i=375;
+        double d=i;
+        System.out.println(i);
+        System.out.println(d);
+          
+
+        double dd=345.66;
+        int ii= (int)dd;
+        System.out.println(dd);
+        System.out.println(ii);
+
+    }
+}
