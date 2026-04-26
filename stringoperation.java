16c91b51d2cbd4e8d0c19156d1dbccba98dc25e6
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-04-26 21:51:56 +0530

    starfor

---
+++ stringoperation.java
@@ -0,0 +1,30 @@
+public class stringoperation {
+    public static void main(String[] args) {
+        String name="Sathwika";
+        String fname="Rathkanthi   ";
+    System.out.println(fname.length());
+    System.out.println(name.length());
+    System.out.println(fname.toLowerCase());
+    System.out.println(name.toLowerCase());
+    System.out.println(fname.toUpperCase());
+    System.out.println(name.toUpperCase());
+    System.out.println(fname.indexOf(" "));
+    System.out.println(name.indexOf('k'));
+    System.out.println(name.charAt(1));
+    System.out.println(name.charAt(0));
+    System.out.println(fname.trim());
+    System.out.println(name+" "+fname);
+    System.out.println(name.equals(fname));
+    System.out.println("Rathkanthi Sathwika \'Gupta\'");
+    System.out.println("Rathkanthi Sathwika \"Gupta\"");
+    System.out.println("Rathkanthi Sathwika \nGupta");
+    System.out.println("Rathkanthi Sathwika \tGupta");
+    System.out.println("Rathkanthi Sathwika \bGupta");
+
+
+
+
+
+    }
+    
+}
