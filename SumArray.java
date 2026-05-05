1601c5ac5a4d49898978c6fc7d7ff17e1781d88a
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-05-05 15:06:19 +0530

    SumArray

---
+++ SumArray.java
@@ -0,0 +1,11 @@
+public class SumArray {
+    public static void main(String[] args) {
+        int sum=0;
+        int[] marks={20,30,40,50};
+        for (int i=0;i<marks.length;i++){
+            sum=sum+marks[i];
+
+        }
+        System.out.println(sum);
+    }
+}
