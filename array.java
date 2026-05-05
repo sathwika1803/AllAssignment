e05efb8a4dee9361fa256cd2ef684d59b02eb6df
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-05-05 08:57:13 +0530

    array

---
+++ array.java
@@ -0,0 +1,17 @@
+public class array {
+    public static void main(String[] args) {
+        int[] ages= new int[5];
+        ages[0]=10;
+        ages[1]=20;
+        ages[2]=30;
+        ages[3]=40;
+        ages[4]=50;
+        System.out.println(ages[0]);
+        System.out.println(ages[1]);
+        System.out.println(ages[2]);
+        System.out.println(ages[3]);
+        System.out.println(ages[4]);
+        
+
+    }
+}
