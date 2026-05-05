d90a919f78a3b53d5a51fbc47b628a9a232e01dc
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-05-05 15:04:06 +0530

    ArrayTraversalForEach

---
+++ ArrayTraversalForEach.java
@@ -0,0 +1,12 @@
+public class ArrayTraversalForEach {
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+
+        System.out.println("Array elements using for-each loop:");
+        for (int value : arr) {
+            System.out.println(value);
+        }
+    }
+}
+    
