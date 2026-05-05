5ea3f6709494a6394191ba2209cc5f31924a4127
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-05-05 09:04:57 +0530

    intArray

---
+++ intArray.java
@@ -0,0 +1,14 @@
+public class intArray {
+    public static void main(String[] args) {
+        int[] ages={21,22,23,24,25};
+        ages[0]=44;
+        ages[3]=56;
+        System.out.println(ages[0]);
+        System.out.println(ages[1]);
+        System.out.println(ages[2]);
+        System.out.println(ages[3]);
+        System.out.println(ages[4]);
+        
+
+    }
+}
