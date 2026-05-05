8bda31fff773297d023c3af4dd159f36b3fae51e
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-05-05 14:35:06 +0530

    forArray

---
+++ forArray.java
@@ -0,0 +1,11 @@
+public class forArray {
+    public static void main(String[] args) {
+        int[] marks={50,23,57,89,54};
+        for(int i=0;i<marks.length;i++){
+            if (marks[i]%2==0){
+                System.out.println(marks[i]);
+            }
+
+        }
+    }
+}
