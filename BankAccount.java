58c53a240321f7e6c03b122b962b048f3d1e763f
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-05-06 11:12:35 +0530

    BankAccount

---
+++ BankAccount.java
@@ -0,0 +1,12 @@
+public class BankAccount {
+    String accountHolder;
+    int Balance;
+    public BankAccount( String accountHolder,int Balance){
+      this.accountHolder=accountHolder;
+      this.Balance=Balance;
+
+    }
+    public static void main(String[] args) {
+        
+    }
+}
