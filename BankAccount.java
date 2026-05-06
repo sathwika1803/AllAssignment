917b42ba1d58a36709905f8c696878594554a745
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-05-06 11:24:43 +0530

    BankAccount1

--- BankAccount.java
+++ BankAccount.java
@@ -7,6 +7,25 @@ public class BankAccount {
 
     }
     public static void main(String[] args) {
-        
+        BankAccount acc1=new BankAccount("Sathwika",3000);
+       acc1.displaybalance();
+
+        acc1.deposit(2000);
+         acc1.withdraw(1000);
+          
     }
+    public void deposit(int amount){
+      Balance=Balance+amount;
+      System.out.println(Balance);
+    }
+    public void withdraw(int amount){
+      Balance=Balance-amount;
+      System.out.println(Balance);
+    }
+     public void displaybalance(){
+      
+      System.out.println(Balance);
+    }
+
+
 }
