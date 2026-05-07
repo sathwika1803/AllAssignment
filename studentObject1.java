26f4b6d0b4e461fb813fb8a1797d911b260e0ccc
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-05-07 22:57:41 +0530

    studentObject1

---
+++ studentObject1.java
@@ -0,0 +1,13 @@
+public class studentObject1 {
+    String name;
+    int roolno;
+    int Marks;
+    public static void main(String[] args) {
+        studentObject1 object=new studentObject1();
+         object.name="Sathwika";
+         object.roolno=23;
+         object.Marks=45;
+         System.out.println(object.name+ " " + object.roolno+  " " + object.Marks);
+
+    }
+}
