38b95c0e4c1ef840e72a3ab9753d0aa19f9004a0
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-05-08 08:22:08 +0530

    two

---
+++ two.java
@@ -0,0 +1,12 @@
+public class two {
+    public static void main(String[] args) {
+        one arithmetic=new one();
+        System.out.println(arithmetic.add(10,30));
+        System.out.println(arithmetic.sub(10,30));
+        System.out.println(arithmetic.mul(10,30));
+        System.out.println(arithmetic.div(10,30));
+        System.out.println(arithmetic.rem(10,30));
+        System.out.println(arithmetic.floor(10,30));
+
+    }
+}
