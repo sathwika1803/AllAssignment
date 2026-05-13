5d162d5d711269f77e46778d6880695cf49c28da
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-05-13 10:44:54 +0530

    emp1

---
+++ emp1.java
@@ -0,0 +1,10 @@
+public class emp1  {
+    private int salary=50000;
+    private String name="sathwika";
+    public String getName(){
+        return name;
+    }
+    public int getSalary(){
+        return salary;
+    }
+}
