3638bf4421aaf9ceb3cfa16da01890c493e9afd8
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-05-13 10:45:08 +0530

    emp2

---
+++ emp2.java
@@ -0,0 +1,10 @@
+public class emp2 {
+    public static void main(String[] args) {
+        emp1 empp=new emp1();
+        System.out.println("empName: "+empp.getName());
+        System.out.println("empSalary: "+empp.getSalary());
+
+    
+        
+    }
+}
