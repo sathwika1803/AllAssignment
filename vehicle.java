41d1087c5ba123fee3b5c826c885fd34d6fc0da8
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-05-14 08:00:07 +0530

    vehicle

---
+++ vehicle.java
@@ -0,0 +1,16 @@
+public class vehicle {
+    public String brand;
+    public int speed;
+    public vehicle(String brand,int speed){
+       this.brand=brand;
+       this.speed=speed;
+
+    }
+    public  void brand(){
+        System.out.println(brand);
+    }
+    public  void speed(){
+        System.out.println(speed);
+    }
+}
+
