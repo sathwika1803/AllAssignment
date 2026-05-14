7656b1b3f4aedcdc622c56d9d3ca86452bacf365
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-05-14 08:00:13 +0530

    main

---
+++ main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        vehicle1 c1=new vehicle1("BMW",250,"petrol");
+        
+       c1.brand();
+       c1.speed();
+       c1.fuel();
+    }
+}
