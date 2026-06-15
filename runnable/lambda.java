7b702ac98d68835ebe59716926e2b1ac783647e4
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-06-15 22:06:25 +0530

    runnable

---
+++ runnable/lambda.java
@@ -0,0 +1,9 @@
+package runnable;
+
+public class lambda {
+    public static void main(String[] args) {
+        Runnable r2 = () -> System.out.println("sathwika"); 
+
+r2.run();
+    }
+}
---
+++ runnable/traditional.java
@@ -0,0 +1,20 @@
+package runnable;
+
+public class traditional {
+    public static void main(String[] args) {
+        Runnable r1 = new Runnable() { 
+
+    @Override 
+
+    public void run() { 
+
+        System.out.println("sathwika"); 
+
+    } 
+
+}; 
+
+r1.run();
+
+    }
+}
